07932c6dde1f80d90f19b2b4220ed05edbaa0293
iliasam <iliasam@mail.ru>
2018-09-02 21:53:37 +0300

    Added VGA example for HAL.

---
+++ simple_vga_example_hal/Src/main.c
@@ -0,0 +1,400 @@
+//Important - I have changed SDRAM GPIO pins settings in HAL_FMC_MspInit() 
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+#include "stm32f429_sdram.h"
+#include "stm32f429_lcd.h"
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+DMA2D_HandleTypeDef hdma2d;
+SDRAM_HandleTypeDef hsdram1;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+static void MX_GPIO_Init(void);
+static void MX_FMC_Init(void);
+static void MX_DMA2D_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_FMC_Init();
+  MX_LTDC_Init();
+  MX_DMA2D_Init();
+
+  /* USER CODE BEGIN 2 */
+  SDRAM_Init();
+  
+  HAL_Delay(2000);
+  
+  lcd_draw_test_picture();
+  HAL_Delay(2000);
+  lcd_clear(LCD_COLOR_YELLOW);
+  
+  LCD_DrawFullRect(50, 50, 100, 100, LCD_COLOR_BLUE);
+  LCD_SetBackColor(LCD_COLOR_YELLOW);
+  LCD_SetTextColor(LCD_COLOR_BLACK);
+  LCD_DisplayStringLine(LINE(0), (uint8_t*)"ILIASAM VGA TEST - HAL");
+    
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+    HAL_GPIO_TogglePin(GPIOI, GPIO_PIN_6);
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
+  PeriphClkInitStruct.PLLSAI.PLLSAIN = 201;
+  PeriphClkInitStruct.PLLSAI.PLLSAIR = 4;
+  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_4;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  //HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_HSI, RCC_MCODIV_1);
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* DMA2D init function */
+static void MX_DMA2D_Init(void)
+{
+
+  hdma2d.Instance = DMA2D;
+  hdma2d.Init.Mode = DMA2D_M2M;
+  hdma2d.Init.ColorMode = DMA2D_OUTPUT_RGB565;
+  hdma2d.Init.OutputOffset = 0;
+  hdma2d.LayerCfg[1].InputOffset = 0;
+  hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_RGB565;
+  hdma2d.LayerCfg[1].AlphaMode = DMA2D_NO_MODIF_ALPHA;
+  hdma2d.LayerCfg[1].InputAlpha = 0;
+  if (HAL_DMA2D_Init(&hdma2d) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  if (HAL_DMA2D_ConfigLayer(&hdma2d, 1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+
+/* FMC initialization function */
+static void MX_FMC_Init(void)
+{
+  FMC_SDRAM_TimingTypeDef SdramTiming;
+
+  /** Perform the SDRAM1 memory initialization sequence
+  */
+  hsdram1.Instance = FMC_SDRAM_DEVICE;
+  /* hsdram1.Init */
+  hsdram1.Init.SDBank = FMC_SDRAM_BANK2;
+  hsdram1.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_8;
+  hsdram1.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_12;
+  hsdram1.Init.MemoryDataWidth = FMC_SDRAM_MEM_BUS_WIDTH_16;
+  hsdram1.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
+  hsdram1.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_2;
+  hsdram1.Init.WriteProtection = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
+  hsdram1.Init.SDClockPeriod = FMC_SDRAM_CLOCK_PERIOD_2;
+  hsdram1.Init.ReadBurst = FMC_SDRAM_RBURST_ENABLE;
+  hsdram1.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_1;
+  /* SdramTiming */
+  SdramTiming.LoadToActiveDelay = 2;
+  SdramTiming.ExitSelfRefreshDelay = 6;
+  SdramTiming.SelfRefreshTime = 4;
+  SdramTiming.RowCycleDelay = 6;
+  SdramTiming.WriteRecoveryTime = 2;
+  SdramTiming.RPDelay = 2;
+  SdramTiming.RCDDelay = 2;
+
+  if (HAL_SDRAM_Init(&hsdram1, &SdramTiming) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/** Configure pins
+     PE4   ------> DCMI_D4
+     PE5   ------> DCMI_D6
+     PE6   ------> DCMI_D7
+     PA4   ------> DCMI_HSYNC
+     PA6   ------> DCMI_PIXCK
+     PH12   ------> DCMI_D3
+     PC8   ------> SDIO_D0
+     PC9   ------> SDIO_D1
+     PA8   ------> RCC_MCO_1
+     PA10   ------> DCMI_D1
+     PC10   ------> SDIO_D2
+     PC11   ------> SDIO_D3
+     PC12   ------> SDIO_CK
+     PD2   ------> SDIO_CMD
+     PD3   ------> DCMI_D5
+     PG9   ------> DCMI_VSYNC
+     PG10   ------> DCMI_D2
+     PI5   ------> S_TIM8_CH1
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOI_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pins : PE4 PE5 PE6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DCMI;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA4 PA6 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_6|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DCMI;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PH12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DCMI;
+  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SDIO_CARD_DET_Pin */
+  GPIO_InitStruct.Pin = SDIO_CARD_DET_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(SDIO_CARD_DET_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC8 PC9 PC10 PC11 
+                           PC12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11 
+                          |GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF0_MCO;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PD2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF12_SDIO;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PD3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DCMI;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PG9 PG10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DCMI;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB3 PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : TIM8_CH1_VIDEO_Pin */
+  GPIO_InitStruct.Pin = TIM8_CH1_VIDEO_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF3_TIM8;
+  HAL_GPIO_Init(TIM8_CH1_VIDEO_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PI6 LED_Pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOI, GPIO_PIN_6|LED_Pin, GPIO_PIN_RESET);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ simple_vga_example_hal/Src/stm32f429_lcd.c
@@ -0,0 +1,299 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f429_lcd.h"
+#include "main.h"
+
+
+#define LCD_FRAME_BUFFER        ((uint32_t)0xD0000000)
+
+#define LCD_DEFAULT_FONT        Font16x24
+
+
+LTDC_HandleTypeDef hltdc;
+extern DMA2D_HandleTypeDef hdma2d;
+
+static sFONT *LCD_Currentfonts;
+
+static uint16_t CurrentTextColor   = 0x0000;
+static uint16_t CurrentBackColor   = 0xFFFF;
+
+
+/* LTDC init function */
+void MX_LTDC_Init(void)
+{
+  LTDC_LayerCfgTypeDef pLayerCfg;
+
+  hltdc.Instance = LTDC;
+  hltdc.Init.HSPolarity = LTDC_HSPOLARITY_AL;
+  hltdc.Init.VSPolarity = LTDC_VSPOLARITY_AL;
+  hltdc.Init.DEPolarity = LTDC_DEPOLARITY_AL;
+  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+  hltdc.Init.HorizontalSync = LCD_TIMING_HSYNC;
+  hltdc.Init.VerticalSync = LCD_TIMING_VSYNC;
+  hltdc.Init.AccumulatedHBP = (LCD_TIMING_HSYNC + LCD_TIMING_HBP); // HSync+HBP;
+  hltdc.Init.AccumulatedVBP = (LCD_TIMING_VSYNC + LCD_TIMING_VBP);  // VSync+VBP;
+  hltdc.Init.AccumulatedActiveW = (LCD_TIMING_HSYNC + LCD_TIMING_HBP + LCD_PIXEL_WIDTH); // HSync+HBP+W;
+  hltdc.Init.AccumulatedActiveH = (LCD_TIMING_VSYNC + LCD_TIMING_VBP + LCD_PIXEL_HEIGHT); // VSync+VBP+H;
+  hltdc.Init.TotalWidth = (LCD_TIMING_HSYNC + LCD_TIMING_HBP + LCD_PIXEL_WIDTH + LCD_TIMING_HFP);  // HSync+HBP+W+HFP;
+  hltdc.Init.TotalHeigh = (LCD_TIMING_VSYNC + LCD_TIMING_VBP + LCD_PIXEL_HEIGHT + LCD_TIMING_VFP);  // VSync+VBP+H+VFP;
+  hltdc.Init.Backcolor.Blue = 0;
+  hltdc.Init.Backcolor.Green = 0;
+  hltdc.Init.Backcolor.Red = 0;
+  if (HAL_LTDC_Init(&hltdc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  pLayerCfg.WindowX0 = 0;
+  pLayerCfg.WindowX1 = LCD_PIXEL_WIDTH;
+  pLayerCfg.WindowY0 = 0;
+  pLayerCfg.WindowY1 = LCD_PIXEL_HEIGHT;
+  
+#if (LCD_BYTES_IN_PIXEL == 2)
+  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_RGB565;
+#elif (LCD_BYTES_IN_PIXEL == 1)
+  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_L8;
+#else
+  #error "ERROR!"
+#endif
+
+  pLayerCfg.Alpha = 255;
+  pLayerCfg.Alpha0 = 0;
+  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_CA;
+  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_CA;
+  pLayerCfg.FBStartAdress = LCD_FRAME_BUFFER;
+  pLayerCfg.ImageWidth = LCD_PIXEL_WIDTH;
+  pLayerCfg.ImageHeight = LCD_PIXEL_HEIGHT;
+  pLayerCfg.Backcolor.Blue = 0;
+  pLayerCfg.Backcolor.Green = 0;
+  pLayerCfg.Backcolor.Red = 0;
+  if (HAL_LTDC_ConfigLayer(&hltdc, &pLayerCfg, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  LCD_SetFont(&LCD_DEFAULT_FONT); /* Set default font */  
+}
+
+/**
+  * @brief  Displays a full rectangle.
+  * @param  Xpos: specifies the X position, can be a value from 0 to 240.
+  * @param  Ypos: specifies the Y position, can be a value from 0 to 320.
+  * @param  Height: rectangle height.
+  * @param  Width: rectangle width.
+  * @retval None
+  */
+void LCD_DrawFullRect(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, uint16_t color)
+{
+  uint32_t color32 =  lcd_convert_to_color32(color);
+
+  uint32_t addr = 0;
+  addr = (hltdc.LayerCfg[0].FBStartAdress) + LCD_BYTES_IN_PIXEL * (ypos * hltdc.LayerCfg[0].ImageWidth + xpos);
+  hdma2d.Init.Mode = DMA2D_R2M;
+  //смещение = ширина экрана минус ширина прямоугольника
+  hdma2d.Init.OutputOffset = hltdc.LayerCfg[0].ImageWidth - width;
+  if(HAL_DMA2D_Init(&hdma2d) == HAL_OK)
+  {
+    if (HAL_DMA2D_Start(&hdma2d, color32, addr, width, height) == HAL_OK)
+    {
+      HAL_DMA2D_PollForTransfer(&hdma2d, 10);
+    }
+  }
+}
+
+
+/**
+  * @brief  Clears the whole LCD.
+  * @param  Color: the color of the background.
+  * @retval None
+  */
+void lcd_clear(uint16_t color)
+{
+  uint32_t color32 =  lcd_convert_to_color32(color);
+    
+  hdma2d.Init.Mode = DMA2D_R2M;
+  hdma2d.Init.OutputOffset = 0;
+  if(HAL_DMA2D_Init(&hdma2d) == HAL_OK)
+  {
+    if (HAL_DMA2D_Start(&hdma2d, color32, LCD_FRAME_BUFFER,
+        LCD_PIXEL_WIDTH, LCD_PIXEL_HEIGHT) == HAL_OK)
+    {
+      HAL_DMA2D_PollForTransfer(&hdma2d, 10);
+    }
+  }
+}
+
+/**
+  * @brief  Sets the LCD Text and Background colors.
+  * @param  TextColor: specifies the Text Color.
+  * @param  BackColor: specifies the Background Color.
+  * @retval None
+  */
+void LCD_SetColors(uint16_t TextColor, uint16_t BackColor)
+{
+  CurrentTextColor = TextColor; 
+  CurrentBackColor = BackColor;
+}
+
+/**
+  * @brief  Gets the LCD Text and Background colors.
+  * @param  TextColor: pointer to the variable that will contain the Text 
+            Color.
+  * @param  BackColor: pointer to the variable that will contain the Background 
+            Color.
+  * @retval None
+  */
+void LCD_GetColors(uint16_t *TextColor, uint16_t *BackColor)
+{
+  *TextColor = CurrentTextColor;
+  *BackColor = CurrentBackColor;
+}
+
+/**
+  * @brief  Sets the Text color.
+  * @param  Color: specifies the Text color code RGB(5-6-5).
+  * @retval None
+  */
+void LCD_SetTextColor(uint16_t Color)
+{
+  CurrentTextColor = Color;
+}
+
+/**
+  * @brief  Sets the Background color.
+  * @param  Color: specifies the Background color code RGB(5-6-5).
+  * @retval None
+  */
+void LCD_SetBackColor(uint16_t Color)
+{
+  CurrentBackColor = Color;
+}
+
+/**
+  * @brief  Sets the Text Font.
+  * @param  fonts: specifies the font to be used.
+  * @retval None
+  */
+void LCD_SetFont(sFONT *fonts)
+{
+  LCD_Currentfonts = fonts;
+}
+
+/**
+  * @brief  Gets the Text Font.
+  * @param  None.
+  * @retval the used font.
+  */
+sFONT *LCD_GetFont(void)
+{
+  return LCD_Currentfonts;
+}
+
+/**
+  * @brief  Draws a character on LCD.
+  * @param  Xpos: the Line where to display the character shape.
+  * @param  Ypos: start column address.
+  * @param  c: pointer to the character data.
+  * @retval None
+  */
+void LCD_DrawChar(uint16_t Xpos, uint16_t Ypos, const uint16_t *c)
+{
+  uint32_t index = 0, counter = 0, xpos =0;
+  uint32_t  Xaddress = 0;
+  
+  xpos = Xpos*LCD_PIXEL_WIDTH*2;
+  Xaddress += Ypos;
+  
+  for(index = 0; index < LCD_Currentfonts->Height; index++)
+  {
+    for(counter = 0; counter < LCD_Currentfonts->Width; counter++)
+    {
+      if((((c[index] & ((0x80 << ((LCD_Currentfonts->Width / 12 ) * 8 ) ) >> counter)) == 0x00) &&(LCD_Currentfonts->Width <= 12))||
+        (((c[index] & (0x1 << counter)) == 0x00)&&(LCD_Currentfonts->Width > 12 )))
+      {
+          /* Write data value to all SDRAM memory */
+         *(__IO uint16_t*) (LCD_FRAME_BUFFER + (2*Xaddress) + xpos) = CurrentBackColor;
+      }
+      else
+      {
+          /* Write data value to all SDRAM memory */
+         *(__IO uint16_t*) (LCD_FRAME_BUFFER + (2*Xaddress) + xpos) = CurrentTextColor;         
+      }
+      Xaddress++;
+    }
+      Xaddress += (LCD_PIXEL_WIDTH - LCD_Currentfonts->Width);
+  }
+}
+
+/**
+  * @brief  Displays one character (16dots width, 24dots height).
+  * @param  Line: the Line where to display the character shape .
+  *   This parameter can be one of the following values:
+  *     @arg Linex: where x can be 0..29
+  * @param  Column: start column address.
+  * @param  Ascii: character ascii code, must be between 0x20 and 0x7E.
+  * @retval None
+  */
+void LCD_DisplayChar(uint16_t Line, uint16_t Column, uint8_t Ascii)
+{
+  Ascii -= 32;
+
+  LCD_DrawChar(Line, Column, &LCD_Currentfonts->table[Ascii * LCD_Currentfonts->Height]);
+}
+
+/**
+  * @brief  Displays a maximum of 20 char on the LCD.
+  * @param  Line: the Line where to display the character shape .
+  *   This parameter can be one of the following values:
+  *     @arg Linex: where x can be 0..9
+  * @param  *ptr: pointer to string to display on LCD.
+  * @retval None
+  */
+void LCD_DisplayStringLine(uint16_t Line, uint8_t *ptr)
+{  
+  uint16_t refcolumn = 0;
+  /* Send the string character by character on lCD */
+  while ((refcolumn < LCD_PIXEL_WIDTH) && ((*ptr != 0) & 
+      (((refcolumn + LCD_Currentfonts->Width) & 0xFFFF) >= LCD_Currentfonts->Width)))
+  {
+    /* Display one character on LCD */
+    LCD_DisplayChar(Line, refcolumn, *ptr);
+    /* Decrement the column position by width */
+    refcolumn += LCD_Currentfonts->Width;
+    /* Point on the next character */
+    ptr++;
+  }
+}
+
+//Convert from 565 to 888
+uint32_t lcd_convert_to_color32(uint16_t color)
+{
+  uint8_t r5 = color >> 11;
+  uint8_t g6 = (color & 0x7E0) >> 5;
+  uint8_t b5 = color & 0x1F;
+  
+  uint8_t r8 = ( r5 << 3 ) | (r5 >> 2);
+  uint8_t g8 = ( g6 << 2 ) | (g6 >> 4);
+  uint8_t b8 = ( b5 << 3 ) | (b5 >> 2);
+  
+  return ((uint32_t)r8 << 16) | ((uint32_t)g8 << 8) | (uint32_t)b8;
+}
+
+void lcd_draw_test_picture(void)
+{
+  uint16_t* test_ptr = (uint16_t*)LCD_FRAME_BUFFER;
+  
+  uint16_t x;
+  uint16_t y;
+  
+  for (y=0; y<LCD_PIXEL_HEIGHT; y++)
+  {
+    for (x=0; x < LCD_PIXEL_WIDTH; x++)
+    {
+      if (x & 32)
+        *test_ptr = 0xFFFF;
+      else
+        *test_ptr = 0;
+      test_ptr++;
+    }
+  }
+}
